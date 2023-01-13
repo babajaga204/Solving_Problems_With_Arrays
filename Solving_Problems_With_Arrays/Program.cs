e2d1d0bd258b4450b53c837969c9df6ca7de7624
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-13 13:18:02 +0100

    Add project files.

---
+++ Solving_Problems_With_Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Solving_Problems_With_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
