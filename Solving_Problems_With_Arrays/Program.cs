907598d20ff3390d1bb09b082ebc93dabfe89b6b
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-16 10:41:51 +0100

    Rewrote encode and decode algorithms to one function that takes in bool param to decide which direction to go

--- Solving_Problems_With_Arrays/Program.cs
+++ Solving_Problems_With_Arrays/Program.cs
@@ -11,45 +11,51 @@ namespace Solving_Problems_With_Arrays
             //int[] nums2 = { 4,5,9,10 };
             //Exercise3_3(nums1, nums1.Length); // Expected false
             //Exercise3_3(nums2, nums2.Length); // Expected true
-            string decipheredText = Encode("HeyWhatsUp");
-            Console.WriteLine(decipheredText);
-            string decoded = Decode(decipheredText);
-            Console.WriteLine(decoded);
+            //string decipheredText = Encode("HeyWhatsUp");
+            //Console.WriteLine(decipheredText);
+            //string decoded = Decode(decipheredText);
+            //Console.WriteLine(decoded);
+
+            EncryptAlgo(true, "hey hva skjer");
+            EncryptAlgo(false, "LCW LTM IHJCO");
 
         }
 
-        private static string Encode(string plaintext)
+        private static void EncryptAlgo(bool encrypt, string plaintext)
         {
             string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             string key = "MNBVCXZLKJHGFDSAPOIUYTREWQ";
-            plaintext = plaintext.ToUpper();
-            char[] codeArray = new char[plaintext.Length];
+            var ciphertext = Encrypt(plaintext, 
+                encrypt ? alphabet : key,
+                encrypt ? key : alphabet);
 
-            for (var i = 0; i < plaintext.Length; i++)
-            {
-                int cipherIndex = alphabet.IndexOf(plaintext[i]);
-                codeArray[i] = key[cipherIndex];
-            }
-
-            string code = new string(codeArray);
-            return code;
+            Console.WriteLine(ciphertext);
         }
 
-        private static string Decode(string code)
-        {
-            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string key = "MNBVCXZLKJHGFDSAPOIUYTREWQ";
-            code = code.ToUpper();
-            char[] charArray = new char[code.Length];
 
-            for (var i = 0; i < code.Length; i++)
+        private static string Encrypt(string plaintext, string alphabet, string key)
+        {
+            var ciphertext = "";
+            for (int i = 0; i < plaintext.Length; i++)
             {
-                int decipherIndex = key.IndexOf(code[i]);
-                charArray[i] = alphabet[decipherIndex];
+                ciphertext += EncryptChar(plaintext[i], alphabet, key);
+            }
+            return ciphertext;
+        }
+
+        private static char EncryptChar(char c, string alphabet, string key)
+        {
+
+            for (var i = 0; i < alphabet.Length; i++)
+            {
+                if (alphabet[i] == char.ToUpper(c))
+                {
+                    return key[i];
+
+                }
             }
 
-            string decipherCode = new string(charArray);
-            return decipherCode;
+            return c;
         }
 
         private static bool Exercise3_3(int[] array, int arraySize)
